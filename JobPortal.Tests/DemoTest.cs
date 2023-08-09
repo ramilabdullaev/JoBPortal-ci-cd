79a0139742cd4fda9ea8471c9f66f53b0cd206ee
Ramil <ramil-1988@outlook.com>
2023-08-09 14:48:14 +0300

    Added integ test

--- JobPortal.Tests/DemoTest.cs
+++ JobPortal.Tests/DemoTest.cs
@@ -1,3 +1,4 @@
+using JobPortal.Controllers;
 using JobPortal.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -15,12 +16,28 @@ namespace JobPortal.Tests
         [Fact]
         public async Task CustomerIntegrationTest()
         {
-            var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables()
+                .Build();
 
             var optionsBuilder = new DbContextOptionsBuilder<CustomerContext>();
             optionsBuilder.UseSqlite(configuration["ConnectionStrings:DefaultConnection"]);
 
             var context =  new CustomerContext(optionsBuilder.Options);
+            await context.Database.EnsureDeletedAsync();
+            await context.Database.EnsureCreatedAsync();
+
+            //context.Customers.RemoveRange(await context.Customers.ToArrayAsync());
+            //await context.SaveChangesAsync();
+
+            var controller = new CustomerController(context);
+
+            await controller.Add(new Customer() { CustomerName = "FooBar" });
+
+            var result = await controller.GetAll();
+            Assert.Single(result);
+            Assert.Equal("FooBar", result.First().CustomerName);
         }
     }
 }
